111bc1b7ab97d3d5e332623b7c52d80bddd1775b
HornCake <kk.ht02h@gmail.com>
2023-12-11 01:32:11 +0900

    [update] Update default layout

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <NuxtPage />
+    <main>
+      <NuxtPage />
+      <ATopScrollButtton />
+    </main>
   </div>
 </template>
 
---
+++ components/ATopScrollButtton.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="button" @click="onClick()">
+    <div class="arrow"></div>
+    <div class="text">TOP</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const onClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+</script>
+
+<style scoped lang="scss">
+  .button {
+    cursor: pointer;
+    position: fixed;
+    right: 100px;
+    bottom: 50px;
+    width: 50px;
+    height: 60px;
+  }
+  .arrow {
+    width: 30px;
+    height: 30px;
+    border-top: 2px solid $black;
+    border-left: 2px solid $black;
+    box-sizing: border-box;
+    transform: translate(10px, 10px) rotate(45deg);
+  }
+  .text {
+    width: 100%;
+    text-align: center;
+  }
+</style>
