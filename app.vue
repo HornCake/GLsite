6d26b6a0e294d7cffb547348c2a67f0c81a739c1
HornCake <kk.ht02h@gmail.com>
2023-12-12 18:29:29 +0900

    [update] Add temporary OGP

--- app.vue
+++ app.vue
@@ -8,7 +8,17 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  useHead({
+    meta: [
+      { name: "title", content: "GL Competition 2023" },
+      { property: "og:title", content: "GL Competition 2023" },
+      { property: "og:description", content: "GL Competition 2023の公式ウェブサイトです" },
+      { property: "og:image", content: "ogpImage" },
+      { property: "og:url", content: "https://www.gl-competition.net/" },
+    ],
+  });
+</script>
 
 <style scoped lang="scss">
   main {
