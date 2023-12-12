664d34c462e54877fff11ded1c516443b73dadce
HornCake <kk.ht02h@gmail.com>
2023-12-12 19:33:55 +0900

    [add] Create scroll anchor system

---
+++ components/AScrollAnchorHeading.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2 ref="heading">{{ text }}</h2>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    text: string;
+  }>();
+  const heading = ref<HTMLElement | null>(null);
+  onMounted(() => {
+    setScrollData(props.text, heading.value);
+  });
+</script>
+
+<style scoped lang="scss"></style>
