966e78547c8819d15846c918fd83e284668d2a51
HornCake <kk.ht02h@gmail.com>
2023-12-15 00:56:28 +0900

    [update] Add scroll link

--- components/OEntry.vue
+++ components/OEntry.vue
@@ -2,12 +2,16 @@
   <section>
     <AScrollAnchorHeading text="応募" />
     <section>
-      <p>応募要項をご確認の上、下のボタンよりエントリーフォームを記入してください。</p>
+      <p><NuxtLink to="/" @click="onClick">応募要項</NuxtLink>をご確認の上、下のボタンよりエントリーフォームを記入してください。</p>
     </section>
   </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const onClick = () => {
+    scrollToEle("応募要項");
+  };
+</script>
 
 <style scoped lang="scss">
   p {
