11af80509b8ab184c3c4ac26e883ff126c99e95c
HornCake <kk.ht02h@gmail.com>
2023-12-17 23:34:06 +0900

    [add] Add footer

---
+++ components/OFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>© 2023 Ground Level</footer>
+</template>
+
+<script setup lang="ts">
+  const menuActive = ref(false);
+
+  const toggle = () => {
+    menuActive.value = !menuActive.value;
+  };
+</script>
+
+<style scoped lang="scss">
+  footer {
+    height: 30px;
+    background-color: $header_black;
+    color: $text_white;
+    font-family: futura-pt, dnp-shuei-gothic-kin-std, sans-serif;
+    text-align: center;
+    padding-top: 7px;
+  }
+</style>
